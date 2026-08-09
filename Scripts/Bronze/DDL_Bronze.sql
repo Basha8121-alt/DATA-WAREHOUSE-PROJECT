19fc8a2a7a2162cf45bea4beb0fbb7005744d991
Basha8121-alt <fazulun8121@gmail.com>
2026-08-10 01:47:44 +0530

    Update DDL_Bronze.sql

--- Scripts/Bronze/DDL_Bronze.sql
+++ Scripts/Bronze/DDL_Bronze.sql
@@ -1,6 +1,10 @@
 -- *** Creating tables for data ingetion from source systems 'crm' & 'erp' ***
 
 -- *** Creating crm source table for cust_info ***
+-- *** Before creating need to check if this table already exists by using T-SQL, IF Exists we are dropping it ***
+IF OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
+DROP TABLE bronze.crm_cust_info;
+
 CREATE TABLE bronze.crm_cust_info(
 	cst_id INT,
 	cst_key NVARCHAR(50),
@@ -12,46 +16,61 @@ CREATE TABLE bronze.crm_cust_info(
 );
 
 -- *** Creating crm source table for prd_info ***
-CREATE TABLE bronze.crm_prd_info(
-prd_id INT,
-prd_key NVARCHAR(50),
-prd_nm NVARCHAR(50),
-prd_cost INT,
-prd_line NVARCHAR(50),
-prd_start_dt DATETIME,
-prd_end_dt DATETIME
+-- *** Before creating need to check if this table already exists by using T-SQL, IF Exists we are dropping it ***
+IF OBJECT_ID ('bronze.crm_prd_info','U') IS NOT NULL
+DROP TABLE bronze.crm_prd_info;
+	CREATE TABLE bronze.crm_prd_info(
+		prd_id INT,
+		prd_key NVARCHAR(50),
+		prd_nm NVARCHAR(50),
+		prd_cost INT,
+		prd_line NVARCHAR(50),
+		prd_start_dt DATETIME,
+		prd_end_dt DATETIME
 );
 
 -- *** Creating crm source table for sales_details ***
-CREATE TABLE bronze.crm_sales_details(
-sls_ord_num INT,
-sls_prd_key NVARCHAR(50),
-sls_cust_id INT,
-sls_order_dt NVARCHAR(50),
-sls_ship_dt NVARCHAR(50),
-sls_due_dt NVARCHAR(50),
-sls_sales INT,
-sls_quantity INT,
-sls_price INT
+-- *** Before creating need to check if this table already exists by using T-SQL, IF Exists we are dropping it ***
+IF OBJECT_ID ('bronze.crm_sales_details','U') IS NOT NULL
+DROP TABLE bronze.crm_sales_details;
+	CREATE TABLE bronze.crm_sales_details(
+		sls_ord_num INT,
+		sls_prd_key NVARCHAR(50),
+		sls_cust_id INT,
+		sls_order_dt NVARCHAR(50),
+		sls_ship_dt NVARCHAR(50),
+		sls_due_dt NVARCHAR(50),
+		sls_sales INT,
+		sls_quantity INT,
+		sls_price INT
 );
 
 -- *** Creating erp source table for CUST_AZ12 ***
-CREATE TABLE bronze.erp_CUST_AZ12(
-CID NVARCHAR(50),
-BDATE DATE,
-GEN NVARCHAR(50)
+-- *** Before creating need to check if this table already exists by using T-SQL, IF Exists we are dropping it ***
+IF OBJECT_ID ('bronze.erp_CUST_AZ12','U') IS NOT NULL
+DROP TABLE bronze.erp_CUST_AZ12;
+	CREATE TABLE bronze.erp_CUST_AZ12(
+		CID NVARCHAR(50),
+		BDATE DATE,
+		GEN NVARCHAR(50)
 );
 
 -- *** Creating erp source table for LOC_A101 ***
-CREATE TABLE bronze.erp_LOC_A101(
-CID NVARCHAR(50),
-CNTRY NVARCHAR(50)
+-- *** Before creating need to check if this table already exists by using T-SQL, IF Exists we are dropping it ***
+IF OBJECT_ID ('bronze.erp_LOC_A101','U') IS NOT NULL
+DROP TABLE bronze.erp_LOC_A101;
+	CREATE TABLE bronze.erp_LOC_A101(
+		CID NVARCHAR(50),
+		CNTRY NVARCHAR(50)
 );
 
 -- *** Creating erp source table for PX_CAT_G1V2 ***
-CREATE TABLE bronze.erp_PX_CAT_G1V2(
-ID NVARCHAR(50),
-CAT NVARCHAR(50),
-SUBCAT NVARCHAR(50),
-MAINTENANCE NVARCHAR(50)
+-- *** Before creating need to check if this table already exists by using T-SQL, IF Exists we are dropping it ***
+IF OBJECT_ID ('bronze.erp_PX_CAT_G1V2','U') IS NOT NULL
+DROP TABLE bronze.erp_PX_CAT_G1V2;
+	CREATE TABLE bronze.erp_PX_CAT_G1V2(
+		ID NVARCHAR(50),
+		CAT NVARCHAR(50),
+		SUBCAT NVARCHAR(50),
+		MAINTENANCE NVARCHAR(50)
 );
